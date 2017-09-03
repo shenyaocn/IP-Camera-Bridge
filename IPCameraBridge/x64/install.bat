8345aec255dd7caff9003988d0dfea4bd881b349
Shen Yao <shenyaocn@users.noreply.github.com>
2017-09-03 19:32:32 +0800

    Add files via upload

---
+++ IPCameraBridge/x64/install.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:: BatchGotAdmin
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+set INSDIR="%ProgramFiles%\IPCameraBridge\x64"
+set INSDLL="%ProgramFiles%\IPCameraBridge\x64\IPCameraBridge.dll"
+MD %INSDIR%
+COPY /Y IPCameraBridge.dll %INSDIR%
+REGSVR32.EXE /S %INSDLL%
+RUNDLL32.EXE %INSDLL%,ShowSettings
+pause
